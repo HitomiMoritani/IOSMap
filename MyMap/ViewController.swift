b1e870938d170b9caa925b8f2d2241c080a2a0f1
森谷　仁美 <hitomi_moritani@jmas.co.jp>
2021-04-06 22:53:54 +0900

    Initial Commit

---
+++ MyMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMap
+//
+//  Created by 森谷仁美 on 2021/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
